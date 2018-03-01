47f4ad862f96527053fd4ccc26a357ed07f439e4
maistrstyk <31645035+it1508@users.noreply.github.com>
2018-03-01 19:19:07 +0100

    Add files via upload

---
+++ git skola/cviko2/main.cpp
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: student
+ *
+ * Created on 16. února 2018, 11:41
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+#include "nevim.h"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int pocet = 0;
+    char input[256];
+    cout << "jak velky char chces, napis cislo: " << endl;
+    cin >> pocet;
+    nevim ch6(pocet);
+    cout << "napis vetu kterou chces vypsat: ";
+    cin >> input;
+    
+    nevim ch7;
+    
+    //ch7 = input;
+    ch7.vypis();
+    nevim ch1;
+    nevim ch2(10);
+    cout << "ch1 vypsani: "<< endl ;
+    ch1.napln();
+    ch1.vypis();
+    ch2.napln();
+    cout << "ch2 vypsani: "<< endl ;
+    ch2.vypis();
+    nevim ch3(10);
+    ch3 = ch2;
+    cout << "ch3 vypsani: "<< endl ;
+    ch3.vypis();
+    cout << "ch4 vypsani: "<< endl ;
+    nevim ch4(1);
+    ch4.napln();
+    ch4 += ch2;
+    ch4.vypis();
+    cout << "ch5 vypsani: "<< endl ;
+    nevim ch5(10);
+    ch5.napln();
+    ch5 + ch2;
+    ch5.vypis();
+    return 0;
+}
+
---
+++ git skola/cviko2/nevim.cpp
@@ -0,0 +1,133 @@
+/* To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   nevim.cpp
+ * Author: student
+ * 
+ * Created on 16. února 2018, 11:41
+ */
+
+#include "nevim.h"
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+
+nevim::nevim() {
+    this->adresa = "";
+    this->pocet_prvku = 0;
+}
+
+nevim::nevim(int pocet){
+    if(pocet <= 0) {
+        this->adresa = "";
+        this->pocet_prvku = 0;
+    }
+    else {
+        this->adresa = new char[pocet];
+        if(this->adresa != "") {
+            this->pocet_prvku = pocet;
+        }
+        else 
+            this->pocet_prvku = 0;
+    }
+}
+
+nevim::nevim(const nevim& orig) {
+    this->adresa = new char[orig.getPocetPrvku()];
+    
+    if(this->adresa != nullptr) {
+        std::strcpy(this->adresa,orig.vypisStringu());
+    }
+}
+
+char * nevim::vypisStringu() const{
+    return this->adresa;
+}
+
+nevim::~nevim() {
+}
+
+bool nevim::napln() {
+    if(this->adresa != "") {
+        for(int i = 0; i < this->pocet_prvku; i++) {
+            this->adresa[i] = '1';
+        }
+        return true;
+    }
+    else 
+        return false;
+}
+
+void nevim::vypis() const {
+    if(this->adresa != "") {
+        for(int i = 0; i < this->pocet_prvku; i++) {
+            std::cout << this->adresa[i] << std::endl;
+        }
+    }
+    else 
+        std::cout << "Pole neexistuje." << std::endl;
+}
+
+char nevim::operator[](int index) const{
+    if(index >= 0 && index < this->pocet_prvku)
+        return this->adresa[index];
+    else {
+        return -1;
+    }
+}
+
+int nevim::getPocetPrvku() const{
+    return this->pocet_prvku;
+}
+
+
+
+nevim &nevim::operator = (const nevim& vpravo){
+    if(this != &vpravo){
+        delete[]this->adresa; //smazat pamet puvodniho objektu
+        
+        this->adresa = new char[vpravo.getPocetPrvku()];
+        
+        if(this->adresa != ""){
+            for(int i = 0;i<vpravo.getPocetPrvku();i++){
+                this->adresa[i]=vpravo[i];
+                this->pocet_prvku = vpravo.getPocetPrvku();
+            }
+        }else
+            this->pocet_prvku =0;    
+        
+    }
+    return *this;
+}
+
+nevim &nevim::operator +=(const nevim& vpravo){
+    char *temp = new char[this->pocet_prvku+vpravo.getPocetPrvku()];
+    if(temp != ""){
+        int i =0;
+        for(;i< this->getPocetPrvku();i++){
+               temp[i] = this->adresa[i];
+
+        }
+
+        for(;i<this->pocet_prvku + vpravo.getPocetPrvku();i++){
+            temp[i] = vpravo[i-this->pocet_prvku];
+        }
+        
+        delete[] this->adresa;
+        this->adresa = temp;
+        this->pocet_prvku = this->pocet_prvku+vpravo.getPocetPrvku();
+        this->adresa[i] = '\0';
+    }
+    
+   
+    return *this;
+}
+
+nevim nevim::operator +(const nevim& vpravo) const{
+    nevim temp(*this);
+    temp +=vpravo;
+    return temp;
+}

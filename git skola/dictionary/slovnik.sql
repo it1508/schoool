4c5b276e473b2d18c21baa78f8e81f91293f9568
maistrstyk <31645035+it1508@users.noreply.github.com>
2018-04-09 07:16:03 +0200

    Add files via upload

--- git skola/dictionary/slovnik.sql
+++ git skola/dictionary/slovnik.sql
@@ -2,10 +2,10 @@
 -- version 4.3.11
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 24, 2016 at 11:24 PM
--- Server version: 5.6.24
--- PHP Version: 5.6.8
+-- Počítač: 127.0.0.1
+-- Vytvořeno: Pon 09. dub 2018, 07:08
+-- Verze serveru: 5.6.24
+-- Verze PHP: 5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,52 +17,57 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `slovnik`
+-- Databáze: `slovnik`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `slovnicek`
+-- Struktura tabulky `slovnicek`
 --
 
 CREATE TABLE IF NOT EXISTS `slovnicek` (
   `id` int(11) NOT NULL,
   `cs` varchar(50) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
-  `en` varchar(50) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `en` varchar(50) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL,
+  `de` varchar(50) CHARACTER SET utf8 COLLATE utf8_czech_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `slovnicek`
+-- Vypisuji data pro tabulku `slovnicek`
 --
 
-INSERT INTO `slovnicek` (`id`, `cs`, `en`) VALUES
-(1, 'počítač', 'computer'),
-(4, 'plamen', 'flame'),
-(6, 'oheň', 'fire'),
-(7, 'slovo', 'word'),
-(8, 'léto', 'summer'),
-(9, 'stůl', 'table');
+INSERT INTO `slovnicek` (`id`, `cs`, `en`, `de`) VALUES
+(7, 'slovo', 'word', 'wort'),
+(8, 'léto', 'summer', 'sommer'),
+(9, 'stůl', 'table', 'tisch'),
+(10, 'ahoj', 'hi', ''),
+(11, 'df', 'dffd', 'ddf'),
+(12, 'gfdgf', 'dgfgf', 'fd'),
+(13, 'xc', 'xcv', 'xcv'),
+(14, 'c', 'c', 'c'),
+(15, 'ghfh', 'uizui', 'lkkl'),
+(16, 'hovno', 'shit', 'scheize');
 
 --
--- Indexes for dumped tables
+-- Klíče pro exportované tabulky
 --
 
 --
--- Indexes for table `slovnicek`
+-- Klíče pro tabulku `slovnicek`
 --
 ALTER TABLE `slovnicek`
   ADD PRIMARY KEY (`id`), ADD KEY `slovnicek_cs_idx` (`cs`), ADD KEY `slovnicek_en_idx` (`en`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pro tabulky
 --
 
 --
--- AUTO_INCREMENT for table `slovnicek`
+-- AUTO_INCREMENT pro tabulku `slovnicek`
 --
 ALTER TABLE `slovnicek`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
